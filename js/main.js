fa0690e9fd57ca5db6c10450f1d5b1718184f944
ExodusDG <sanistray@gmail.com>
2022-09-24 18:56:05 +0300

    update

--- js/main.js
+++ js/main.js
@@ -19,3 +19,24 @@ const homepageSwiper = new Swiper('.gallerySlider', {
     spaceBetween: 5,
     centeredSlides: true,
 });
+
+$.each($('.faqItem'), function () {
+    var hiddenHeight = $(this).find('.faqItemVisible').height();
+    var visibleHeight = $(this).find('.faqItemAnswer').height();
+
+    $(this).height(40);
+})
+
+$('.faqItem').click(function () {
+    var H = $(this).height();
+    var hVisible = $(this).find('.faqItemAnswer').height() + 10;
+
+    if ($(this).hasClass('faqOpen')) {
+        $(this).height(40);
+        $(this).removeClass('faqOpen')
+
+    } else {
+        $(this).height(H + hVisible);
+        $(this).addClass('faqOpen')
+    }
+})
